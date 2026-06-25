9705801d755eaec3a60d0edc99e82f7964e97292
Gery <piskov91@gmail.com>
2026-06-25 21:58:43 +0100

    use img tag instead of background-image to fix Android zoom

--- src/components/SiteBackground.tsx
+++ src/components/SiteBackground.tsx
@@ -1,6 +1,3 @@
-/** Served from <code>public/fittingroom.png</code>. */
-const FITTING_ROOM_BG = "/fittingroom.png";
-
 /**
  * Full-viewport photo background + dark gradient overlay behind all pages.
  */
@@ -8,17 +5,17 @@ export function SiteBackground() {
   return (
     <div className="app-bg pointer-events-none fixed inset-0 z-0" aria-hidden>
       <div className="app-bg__base" />
-      <div
-        className="app-bg__photo"
+      <img
+        src="/fittingroom.png"
+        alt=""
+        aria-hidden
         style={{
-          backgroundImage: `url("${FITTING_ROOM_BG}")`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundAttachment: "scroll",
           position: "absolute",
+          inset: 0,
           width: "100%",
           height: "100%",
+          objectFit: "cover",
+          objectPosition: "center",
         }}
       />
       <div className="absolute inset-0 bg-gradient-to-b from-black/82 via-black/52 to-black/78 md:from-black/70 md:via-black/45 md:to-black/68" />
